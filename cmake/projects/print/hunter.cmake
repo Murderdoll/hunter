598a3b8c52df2172906309a6035a3f86a16750e1
Murderdoll <artem765@inbox.ru>
2017-11-03 21:37:38 +0300

    added print package

---
+++ cmake/projects/print/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    print
+    VERSION
+    "0.1.0.0"
+    URL
+    "https://github.com/Murderdoll/lab09/archive/v0.1.0.0.tar.gz"
+    SHA1
+    a46c764ef5f3e043a4baed870dc206cba0ccd989
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    print
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(print)
+hunter_download(PACKAGE_NAME print)
